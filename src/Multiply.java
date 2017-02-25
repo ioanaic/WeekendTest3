ff14a72a02fb5474f309dc15cd2388827ee2b90f
FirstJavaProj <crisan_ioana2005@yahoo.com>
2017-02-25 13:01:25 +0200

    Add new method Multiply: multi(x,y)

---
+++ src/Multiply.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Ioana on 2/25/2017.
+ */
+public class Multiply {
+    public static void main(String[] args) {
+        int x = SkeletonJava.readIntConsole("x");
+        int y = SkeletonJava.readIntConsole("y");
+        int multi = multi(x, y);
+        System.out.println(x + " * " + y + " = " + multi);
+
+    }
+
+    private static int multi(int x, int y) {
+        int max, min, multi;
+        if (x > y) {
+            max = x;
+            min = y;
+        }else{
+            max = y;
+            min = x;
+        }
+      multi = max;
+        for(int counter = 1;counter<min;counter++){
+            multi += max;
+        }
+        return multi;
+    }
+}
