4df6f8eea463995cb31b9de015275c9ee2e03fe4
flo <flo.macicasan@gmail.com>
2017-02-25 10:55:24 +0200

    changed print to new message

--- src/Example.java
+++ src/Example.java
@@ -4,7 +4,7 @@
  */
 public class Example {
     public static void main(String[] thisIsAnArgument) {
-        System.out.println("Hello!");
+        System.out.println("Good bye!");
 
         int sum = Calc.add(2, 3);
         System.out.println(sum);  //5
