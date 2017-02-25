9c804f57fabf78daec2d67c87596c4699f847158
flo <flo.macicasan@gmail.com>
2017-02-25 10:52:45 +0200

    first java program to add 2 numbers

---
+++ src/Calc.java
@@ -0,0 +1,9 @@
+/**
+ * @author flo
+ * @since 25/02/2017.
+ */
+public class Calc {
+    public static int add(int cucurigu , int b) {
+        return cucurigu + b;
+    }
+}
---
+++ src/Example.java
@@ -0,0 +1,17 @@
+/**
+ * @author flo
+ * @since 25/02/2017.
+ */
+public class Example {
+    public static void main(String[] thisIsAnArgument) {
+        System.out.println("Hello!");
+
+        int sum = Calc.add(2, 3);
+        System.out.println(sum);  //5
+    }
+
+}
+
+
+
+
