0d7bbf4434349cd305c2fef8815ca09f56b6562c
FirstJavaProj <crisan_ioana2005@yahoo.com>
2017-02-25 12:30:26 +0200

    Add new method: Sum(x,y)

--- src/Example.java
+++ src/Example.java
@@ -6,33 +6,36 @@ public class Example {
     public static void main(String[] thisIsAnArgument) {
         System.out.println("Good bye!");
 
-        int suma = Calc.add(2, 3);
-        System.out.println(suma);  //5
 
         int x = SkeletonJava.readIntConsole("x=");
         int y = SkeletonJava.readIntConsole("y=");
-int max,min;
-if(x>y){
-    max=x;
-    min=y;
-}
-else{
-    max=y;
-    min=x;
-}
+        int suma = Sum(x, y);
+        SkeletonJava.printConsole(x + "+" + y + "=" + suma);
+    }
+
+    public static int Sum(int x, int y) {
+        int max, min;
+        if (x > y) {
+            max = x;
+            min = y;
+        } else {
+            max = y;
+            min = x;
+        }
 
         int sum = max;
         //int counter = y;
 
         //while (counter > 0) {
-        for(int counter = 0;counter<min;counter++){
+        for (int counter = 0; counter < min; counter++) {
             sum = sum + 1;
             SkeletonJava.printConsole("Adding 1 to sum");
-           // counter = counter - 1;
+            // counter = counter - 1;
         }
-        SkeletonJava.printConsole(x + "+" + y + "=" + sum);
-    }
 
+
+        return sum;
+    }
 }
 
 
