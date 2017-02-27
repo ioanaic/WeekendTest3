064f1cdf1c4c43359d965acee3973591b7d54003
ioanaic <crisan.ioana27@gmail.com>
2017-02-27 17:07:35 +0200

    Add Pb 5 from Lab 3

---
+++ src/TemaPb3Lab3.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ioana.crisan on 27-02-2017.
+ */
+public class TemaPb3Lab3 {
+//    Given an array of numbers, check if the elements are increasingly sorted
+//    (hint) check adjacent pairs for the order relation
+public static void main(String[] args) {
+
+}
+
+}
