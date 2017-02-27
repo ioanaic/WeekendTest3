648e7a0625b3d7b2dfec221bb7696ac2b72cf800
ioanaic <crisan.ioana27@gmail.com>
2017-02-27 15:34:00 +0200

    Modify the methods: evenNumbers, oddNumbers and adding afis(int[] x) method

--- src/TemaPb2Lab3.java
+++ src/TemaPb2Lab3.java
@@ -5,36 +5,73 @@ public class TemaPb2Lab3 {
     //Given an array of numbers, separate elements by parity
     public static void main(String[] args) {
         int n = SkeletonJava.readIntConsole("How many numbers? ");
-        int nEven, nOdd, j, k;
-        j = 0;
-        k = 0;
-        int b[] = new int[n];
-        int c[] = new int[n];
         int a[] = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = SkeletonJava.readIntConsole("a[" + i + "]: ");
 
         }
-        for(int i=0;i<n;i++){
-               if(isEven(a[i])){
-                   j++;
-                   b[j] = a[i];
-                   System.out.println(b[j]);
-               }else{
-                   k++;
-                   c[k] = a[i];
-               }
+        System.out.println("Even numbers are: ");
+        int[] even = evenNumbers(a);
+        afis(even);
 
-        }
+
+        System.out.println("\n Odd numbers are: ");
+        int[] odd = oddNumbers(a);
+        afis(odd);
 
     }
 
-    public static boolean isEven(int no) {
-        if (no % 2 == 0) {
-            return true;
-        } else {
-            return false;
+    public static int[] evenNumbers(int[] x) {
+        int l = x.length;
+        int k = 0;
+        int counter = 0;
+
+        for (int i = 0; i < l; i++) {
+            if (x[i] % 2 == 0) {
+                k++;
+            }
         }
+        int[] e = new int[k];
+        for (int i = 0; i < l; i++) {
+            if (x[i] % 2 == 0) {
+                e[counter] = x[i];
+                counter++;
+            }
+        }
+        return e;
+    }
+
+    public static int[] oddNumbers(int[] x) {
+        int l = x.length;
+        int p = 0;
+        int counter = 0;
+
+        for (int i = 0; i < l; i++) {
+            if (x[i] % 2 != 0) {
+                p++;
+
+            }
+        }
+        int[] o = new int[p];
+        for (int i = 0; i < l; i++) {
+            if (x[i] % 2 != 0) {
+                o[counter] = x[i];
+                counter++;
+            }
+        }
+        return o;
+    }
+
+    public static int[] afis(int[] x) {
+        int len = x.length;
+        if (len > 0) {
+            for (int i = 0; i < len; i++) {
+                System.out.print(x[i] + " ");
+            }
+        } else {
+            System.out.print(" Empty ");
+        }
+        return x;
     }
 
 }
