19584e3840060f2d2ace44251ff2651f5a28964b
ioanaic <crisan.ioana27@gmail.com>
2017-02-28 10:16:37 +0200

    Add Pb 5 from Lab 3

--- src/TemaPb3Lab3.java
+++ src/TemaPb3Lab3.java
@@ -2,13 +2,14 @@
  * Created by ioana.crisan on 27-02-2017.
  */
 public class TemaPb3Lab3 {
-//    Given an array of numbers, check if the elements are increasingly sorted
-//    (hint) check adjacent pairs for the order relation
-public static void main(String[] args) {
-int n = SkeletonJava.readIntConsole("How many numbers? ");
-    for(int i=0;i<n;){
-
+    //    Given an array of numbers, check if the elements are increasingly sorted
+//    (hint) check adjacent pairs for the order relatio
+    public static void main(String[] args) {
+        int n = SkeletonJava.readIntConsole("How many numbers? ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; ) {
+            a[i] = SkeletonJava.readIntConsole("a[" + i + "]: ");
+        }
     }
-}
 
 }
