c9f170328069355faa29443ebdeb4021b386ab5d
FirstJavaProj <crisan_ioana2005@yahoo.com>
2017-02-28 20:28:32 +0200

    Curs 4 - Add First Object  - Person Obj

---
+++ src/QALab/Person.java
@@ -0,0 +1,25 @@
+package QALab;
+
+/**
+ * Created by Ioana on 2/28/2017.
+ */
+public class Person {
+    private String prenume;
+    private String nume;
+    private int ziNastere;
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public void setPrenume(String prenume){
+        this.prenume = prenume;
+    }
+
+    public void setNume(String x){
+        prenume = x;
+    }
+
+
+
+}
