b1ba33712ebc79646af6d36eb71b5bafada21d94
FirstJavaProj <crisan_ioana2005@yahoo.com>
2017-02-28 20:49:25 +0200

    Curs 4 - Add First Object  - Person Obj - Add changes

--- src/QALab/Person.java
+++ src/QALab/Person.java
@@ -8,18 +8,35 @@ public class Person {
     private String nume;
     private int ziNastere;
 
+    public Person() {
+
+    }
+
+    public Person(String nume, String prenume, int x) {
+        this.prenume = prenume;
+        this.nume = nume;
+        ziNastere = x;
+    }
+
     public String getPrenume() {
         return prenume;
     }
 
-    public void setPrenume(String prenume){
+    public void setPrenume(String prenume) {
         this.prenume = prenume;
     }
 
-    public void setNume(String x){
+    public void setNume(String x) {
         prenume = x;
     }
 
-
-
+    public static void main(String[] args) {
+        Person florin = new Person();
+        Person maria = new Person();
+        florin.setNume("Macicasan");
+        florin.setPrenume("Florin");
+        maria.setPrenume("Maria");
+        //suprascriu
+        florin.setNume("Popescu");
+    }
 }
