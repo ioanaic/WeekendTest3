ab306d518cb0abf9b1c14b01c8a879dc415b8c33
FirstJavaProj <crisan_ioana2005@yahoo.com>
2017-02-28 19:54:46 +0200

    Curs 3 - Add new class Victoria

---
+++ src/Victoria.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Ioana on 2/28/2017.
+ */
+import java.lang.String;
+public class Victoria {
+
+    //read until Victoria
+    public static void main(String[] args) {
+        String[] a = new String[100];
+
+//        do{
+// a[i] = SkeletonJava.readStringConsole("a["+i+"]");
+//i++;
+//        }while (a[i]!="Victoria");
+
+//        while (!a.equals("Victoria")){
+//            a = SkeletonJava.readStringConsole(a+" ");
+
+        }
+    }
